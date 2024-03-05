c7c677a0c0cc4105a07f855164003a6c17c42622
saima-salar <157168170+saima-salar@users.noreply.github.com>
2024-03-06 01:24:16 +0500

    Add files via upload

---
+++ class2.ts
@@ -0,0 +1,25 @@
+// let message="Hello World";
+//  console.log(message)
+
+// let message="Hello World";
+// message="Hello Karachi"
+// console.log("Hello Karachi")
+// let firstName:string="Saima"
+// console.log(firstName)
+// let one="11";
+// let two="4";
+// console.log(one + two)
+// let abc:number=2
+// console.log(abc+5)
+// console.log(10-abc)
+// console.log(abc*6)
+// console.log(5/abc)
+let digit:number=10/6*5+3-2
+console.log(digit)
+
+
+
+
+
+
+
